fe69e70ef58dd1993cf08ead3ba8c658eccbb227
Aryan <aryandps06@gmail.com>
2026-07-03 10:13:53 +0530

    Fix Vercel deployment bugs

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static & Media Asset Pipeline Configurations
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
