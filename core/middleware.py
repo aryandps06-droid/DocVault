6b9d9451c1dc176cca04353f05b55dcc682c92c2
Aryan <aryandps06@gmail.com>
2026-07-14 12:50:43 +0530

    fix: skip auto-login on public landing page to allow viewing features section

--- core/middleware.py
+++ core/middleware.py
@@ -5,8 +5,8 @@ class AutoLoginMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # If the user is not authenticated, automatically log them in as the primary superuser
-        if not hasattr(request, 'user') or not request.user.is_authenticated:
+        # If the user is not authenticated and is not on the public landing page, automatically log them in as the primary superuser
+        if request.path != '/' and (not hasattr(request, 'user') or not request.user.is_authenticated):
             User = get_user_model()
             # Find the primary superuser (e.g. aryan9)
             superuser = User.objects.filter(username='aryan9').first() or User.objects.filter(is_superuser=True).first()
